b66a4b16f762ad7e51ed906376144be612e49031
GITA <jgitanjali592@gmail.com>
2024-03-27 10:44:48 +0530

    this code for timeline for loader

--- script.js
+++ script.js
@@ -5,3 +5,26 @@
     delay:0.5
     
  }) 
+
+ var h5timer = document.querySelector("#line1-part1 h5")
+   var grow = 0
+ setInterval(function (){
+   if(grow<100){
+      h5timer.innerHTML = grow++
+   }
+    else{
+      h5timer.innerHTML = grow 
+    }
+ } , 33)
+
+
+ var tl = gsap.timeline()
+  tl.to("#loader" , {
+
+    opacity:0,
+    duration:0.4,
+    delay:4
+
+  })
+ 
+ 
