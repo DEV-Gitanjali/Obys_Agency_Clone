99f6fb4fddff5f2f207ecf800089f896791f5a86
GITA <jgitanjali592@gmail.com>
2024-03-27 13:11:51 +0530

    this code for nav

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
+function loadingAnimation() {
+  var tl = gsap.timeline();
 
-var tl = gsap.timeline()
-
-gsap.from(".line h1" , {
-  y: 150,
-  stagger:0.25,
-  duration:0.6,
-  delay:0.5,
-  
-}) ;
-tl.from("#line1-part1 , .line h2", {
-     opacity:0,
-  onStart: function (){
-    
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
-    setInterval(function (){
-      if(grow<100){
-        h5timer.innerHTML = grow++ ;
-      }
-      else{
-        h5timer.innerHTML = grow;
-      }
-    } , 33);
-
-  },
-});
-
-  tl.to("#loader" , {
-    opacity:0,
-    duration:0.2,
-    delay:4
-
+  gsap.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5
   });
 
-  tl.from("#page1" , {
-    delay:0.2,
-    y:1600,
-    duration:0.5,
-    ease:"power4"
-  })
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function() {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function() {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 33);
+    }
+  });
 
-  tl.to("#loader" , {
-    display:"none"
-  })
+  tl.to('.line h2', {
+    opacity: 1
+  });
 
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 4
+  });
 
- 
- 
+   tl.from("#page1", {
+     delay:0.2,
+     y:1600,
+     duration:0.5,
+     ease:"power4"
+   });
+  
+   tl.to("#loader", {
+     display:"none"
+   });
+}
+
+loadingAnimation();
